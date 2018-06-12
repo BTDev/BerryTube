0ea96eae2f3574f3d84963c4cb01de0017fcb537
Atte Virtanen <aten.email@gmail.com>
2018-06-12 17:47:52 +0000

    Fix database connections during transition period

--- docker/node/bt_data/db_info.js
+++ docker/node/bt_data/db_info.js
@@ -5,7 +5,7 @@ dbcon.host = 'mysql';
 dbcon.post = 3306;
 dbcon.mysql_user = 'berrytube';
 dbcon.mysql_pass = 'berrytube';
-dbcon.database = 'berrytube';
+dbcon.database = process.env.MYSQL_PASSWORD || 'berrytube';
 dbcon.video_table = 'videos';
 
 module.exports = dbcon;
--- web/api/apiconfig.php
+++ web/api/apiconfig.php
@@ -3,6 +3,6 @@
     define("DB_HOST","mysql");
     define("DB_NAME","berrytube");
     define("DB_USER","berrytube");
-    define("DB_PASS",getenv('MYSQL_PASSWORD'));
+    define("DB_PASS",getenv('MYSQL_PASSWORD') ? getenv('MYSQL_PASSWORD') : 'berrytube');
     /* CUT AFTER ME FOR ANY CHANGES. */
     define("PATH","/");
