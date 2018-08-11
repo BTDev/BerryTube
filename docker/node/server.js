b940154e489ef6dcb954778e5e28b9d989988d9d
Atte Virtanen <aten.email@gmail.com>
2018-08-11 11:10:17 +0000

    More robust SIGTERM handling

--- docker/node/server.js
+++ docker/node/server.js
@@ -509,6 +509,7 @@ SERVER.DLOG = fs.createWriteStream(SERVER.settings.core.debug_file_name,{flags: 
 SERVER.FAILED_LOGINS=[];
 SERVER.RECENTLY_REGISTERED=[];
 SERVER.GILDNAME = "*";
+SERVER.TERMINATE_CODE = null;
 
 var MODE_VIDEOCHAT = 0;
 var MODE_CHATONLY = 1;
@@ -1139,6 +1140,10 @@ var commit = function(){
 
 	upsertMisc({name:'server_time', value:''+Math.ceil(SERVER.TIME)});
 	upsertMisc({name:'server_active_videoid', value:''+SERVER.ACTIVE.videoid});
+
+	if (SERVER.TERMINATE_CODE != null) {
+		process.exit(SERVER.TERMINATE_CODE);
+	}
 };
 
 const commitInterval = setInterval(commit,SERVER.settings.core.db_commit_delay);
@@ -1148,10 +1153,8 @@ process.on('SIGTERM', function(signal){
 	console.log('Running commit before exit...');
 	clearInterval(commitInterval);
 	io.sockets.emit('serverRestart');
+	SERVER.TERMINATE_CODE = 128 + signal;
 	commit();
-	setTimeout(function(){
-		process.exit(128 + signal);
-	}, 1000);
 });
 
 function hotPotatoLeader(departing){
