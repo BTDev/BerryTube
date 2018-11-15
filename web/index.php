e260a0398f45d4e44777fe4df6c1fe4b3b2a6531
Atte Virtanen <aten.email@gmail.com>
2018-11-15 14:44:32 +0000

    Add preconnect hints to front page

--- web/index.php
+++ web/index.php
@@ -1,14 +1,21 @@
 <?php
 
+	require_once('config.php');
+
+	if (!NO_CDN) {
+		header('Link: <'.CDN_ORIGIN.'>; rel=preconnect; crossorigin', false);
+	}
+	header('Link: <'.SOCKET_ORIGIN.'>; rel=preconnect; crossorigin', false);
+	header('Link: <https://cdnjs.cloudflare.com>; rel=preconnect; crossorigin', false);
+	header('Link: <https://player.twitch.tv>; rel=preconnect; crossorigin', false);
+	header('Link: <https://player.vimeo.com>; rel=preconnect; crossorigin', false);
+	header('Link: <https://w.soundcloud.com>; rel=preconnect; crossorigin', false);
+	header('Link: <https://www.youtube.com>; rel=preconnect; crossorigin', false);
+	header('Link: <https://api.teamup.com>; rel=preconnect; crossorigin', false);
+
 	if(isset($_GET['LayoutType'])) { $_COOKIE['LayoutType'] = $_GET['LayoutType']; }
-	if(isset($_COOKIE['LayoutType'])){
-		if($_COOKIE['LayoutType'] == "compact"){
-			require_once("compact.php");
-		} elseif($_COOKIE['LayoutType'] == "miggy"){
-			require_once("miggy.php");
-		} else {
-			require_once("hd.php");
-		}
+	if(isset($_COOKIE['LayoutType']) && $_COOKIE['LayoutType'] == "compact"){
+		require_once("compact.php");
 	} else {
 		require_once("hd.php");
 	}
