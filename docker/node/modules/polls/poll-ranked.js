6a94d7236217deccb1ac098c6a4644b36eff6db8
Nelson LaQuet <nelsonlaquet@gmail.com>
2019-03-05 00:49:08 -0600

    Begin work on new poll algorithim

--- docker/node/modules/polls/poll-ranked.js
+++ docker/node/modules/polls/poll-ranked.js
@@ -84,138 +84,26 @@ exports.RankedPoll = class extends PollInstance {
 	calculateResults() {
 		const { options: { options }, votes } = this;
 
-		const finalVoteCounts = options.map((_, i) => ({
-			votes: 0, 
-			index: i, 
-			isExcluded: false, 
-			rankDistribution: [0, 0, 0], 
-			opacity: .2
-		}));
-		
-		const hasApplied = this.votes.map(v => v.optionIndicies.map(() => false));
+		const finalOptions = options
+			.map((_, i) => ({
+				index: i, 
+				isExcluded: false, 
+				rankDistribution: [0, 0, 0],
+				opacity: .2
+			}));
 
-		const finalResultsOrder = [];
-
-		let finalMaxVote;
-		
-		if (votes.length == 0)
-			return finalVoteCounts;
-
-		const optionVoteTotalCount = options.map(() => 0);
-
-		for (const vote of this.votes) {
-			for (let rank = 0; rank < vote.optionIndicies.length; rank++) {
-				const optionIndex = vote.optionIndicies[rank];
-				if (typeof(optionIndex) === "undefined")
-					continue;
-
-				optionVoteTotalCount[optionIndex]++;
-			}
-		}
-
-		let round = 0;
-
-		while (true) {
-			// tally up all votes, except for ones that are excluded
-			const votesForOption = options.map((_, i) => ({votes: 0, index: i}));
-			let totalVotesCast = 0;
-			const hasVoted = this.votes.map(f => false);
-			
-			for (let voteIndex = 0; voteIndex < this.votes.length; voteIndex++) {
-				const vote = this.votes[voteIndex];
-				for (let rank = 0; rank < vote.optionIndicies.length; rank++) {
-					const optionIndex = vote.optionIndicies[rank];
-					if (typeof(optionIndex) === "undefined" || optionIndex === null)
-						continue;
-					
-					const finalVoteObject = finalVoteCounts[optionIndex];
-					if (!hasApplied[voteIndex][rank]) {
-						finalVoteObject.rankDistribution[rank]++;
-						hasApplied[voteIndex][rank] = true;
-					}
-
-					if (finalVoteObject.isExcluded)
-						continue;
-
-					if (!hasVoted[voteIndex]) {
-						totalVotesCast++;
-						votesForOption[optionIndex].votes++;
-						hasVoted[voteIndex] = true;
-					}
+		for (let voteIndex = 0; voteIndex < votes.length; voteIndex++) {
+			const vote = votes[voteIndex];
+			for (let rankIndex = 0; rankIndex < vote.optionIndicies.length; rankIndex++) {
+				const optionIndex = vote.optionIndicies[rankIndex];
+				if (typeof(optionIndex) !== "undefined") {
+					finalOptions[optionIndex].rankDistribution[rankIndex]++;
 				}
 			}
-
-			let minVote = 50000;
-			let maxVote = 0;
-			let maxEligableVote = 0;
-			const twoThirdsCutoff = (2 / 3) * totalVotesCast;
-
-			for (let i = 0; i < votesForOption.length; i++) {
-				if (finalVoteCounts[i].isExcluded)
-					continue;
-
-				const count = votesForOption[i].votes;
-				minVote = Math.min(minVote, count);
-				maxVote = Math.max(maxVote, count);
-
-				const isEligable = 
-					!options[i].isTwoThirds
-					|| count >= twoThirdsCutoff;
-
-				if (isEligable)
-					maxEligableVote = Math.max(maxEligableVote, count);
-			}
-
-			finalMaxVote = maxVote;
-
-			// copy in the latest vote data into our finalVoteCounts
-			let choppingBlock = [];
-			for (let i = 0; i < votesForOption.length; i++) {
-				const option = votesForOption[i];
-				const finalData = finalVoteCounts[i];
-
-				if (finalData.isExcluded)
-					continue;
-
-				// record the highest votes that an option ever got
-				finalData.votes = Math.max(finalData.votes, option.votes);
-
-				if (option.votes == maxEligableVote)
-					continue;
-				
-				choppingBlock.push({ index: i, votes: option.votes });
-			}
-
-			// we are done if nothing was eliminated
-			if (!choppingBlock.length) {
-				// before we break, add our winners to the top of the result set
-				for (let i = 0; i < votesForOption.length; i++) {
-					if (finalVoteCounts[i].isExcluded)
-						continue;
-
-					finalResultsOrder.push(i);
-				}
-				
-				break;
-			}
-
-			// choose an item to elimiate... prefer non-two third options
-			const toEliminateIndex = choppingBlock.sort((l, r) => l.votes - r.votes)[0].index;
-			finalResultsOrder.push(toEliminateIndex);
-			finalVoteCounts[toEliminateIndex].isExcluded = true;
-
-			// as a safegarud...
-			if (++round > 300) {
-				this.log.error(events.EVENT_RUNAWAY_CODE, "we spent {rounds} rounds on this poll :(", { round });
-				throw new Error("Too much loop");
-			}
 		}
 		
-		// pre-calculate the target opacity
-		for (let i = 0; i < finalVoteCounts.length; i++) {
-			finalVoteCounts[i].opacity = Math.max(finalVoteCounts[i].votes / finalMaxVote, .2);
-		}
+		
 
-		return finalResultsOrder.reverse().map(i => finalVoteCounts[i]);
+		return finalResults;
 	}
 };
