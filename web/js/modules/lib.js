6cdb90b83d8975a3027440033a3a958c6652fca5
Atte Virtanen <aten.email@gmail.com>
2019-06-30 18:16:43 +0000

    Run prettier on new code

--- web/js/modules/lib.js
+++ web/js/modules/lib.js
@@ -9,16 +9,16 @@ export function $(...args) {
  * @param {number) num
  */
 export function getOrdinal(num) {
-    switch (num % 10) {
-	case 1:
-	    return num + 'st';
-	case 2:
-	    return num + 'nd';
-	case 3:
-	    return num + 'rd';
-	default:
-	    return num + 'th';
-    }
+	switch (num % 10) {
+		case 1:
+			return num + "st";
+		case 2:
+			return num + "nd";
+		case 3:
+			return num + "rd";
+		default:
+			return num + "th";
+	}
 }
 
 /**
