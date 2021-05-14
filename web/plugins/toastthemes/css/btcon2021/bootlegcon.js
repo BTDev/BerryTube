7874cdd4b48c74b71690d620feeb7eedbbbbca82
Atte Virtanen <aten.email@gmail.com>
2021-05-14 20:15:27 +0300

    fix

--- web/plugins/toastthemes/css/btcon2021/bootlegcon.js
+++ web/plugins/toastthemes/css/btcon2021/bootlegcon.js
@@ -91,7 +91,7 @@ not in sudoers file
     <style>
         #videobg {
             background: url(${
-              beegees + ~~(Math.random() * 13 + 1)
+              beegees + ~~(Math.random() * 15 + 1)
             }.png) no-repeat center center !important;
         }
     </style>
