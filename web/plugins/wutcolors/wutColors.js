679eef0ae48bf751e28da24c5e74680767188f90
Miikka Lahtinen <smidqe@hotmail.fi>
2021-11-13 18:19:18 +0200

    Use = instead of *=

--- web/plugins/wutcolors/wutColors.js
+++ web/plugins/wutcolors/wutColors.js
@@ -92,9 +92,9 @@ function wutProcessUsername(nick){
     wutStyleSheet.insertRule(`.msgwrap[nick="${nick}"] > .message > .nick.flaunt:before {color:${c};transform: rotate(-20deg);position:absolute;top:-15px;left:-15px;content:'\\e900';font-family: 'wut-icons' !important;font-style: normal;font-weight: normal;font-variant: normal;text-transform: none;line-height: 1;font-size: 30px;z-index:-1; }`,0);
     wutStyleSheet.insertRule(`.msgwrap[nick="${nick}"] > .message > .nick.flaunt+.msg:after {content: '';position:absolute;top:0;left:-5%;width:110%;height:100%;z-index:-1;background:rgba(0,0,0,.2);}`,0);
 
-    wutStyleSheet.insertRule(`#chatlist > ul > li[nick*="${nick}"]  { border-style: solid;  border-left-width: 10px ; border-left-color : ${c}; margin-left: 0; padding-left: 5px }`,0);
-    wutStyleSheet.insertRule(`.user[nick*="${nick}"] > .chatlistname { color:${c};  }`,0);
-    wutStyleSheet.insertRule(`.user.leader[nick*="${nick}"] > .chatlistname { padding-left:24px;  }`,0);
+    wutStyleSheet.insertRule(`#chatlist > ul > li[nick="${nick}"]  { border-style: solid;  border-left-width: 10px ; border-left-color : ${c}; margin-left: 0; padding-left: 5px }`,0);
+    wutStyleSheet.insertRule(`.user[nick="${nick}"] > .chatlistname { color:${c};  }`,0);
+    wutStyleSheet.insertRule(`.user.leader[nick="${nick}"] > .chatlistname { padding-left:24px;  }`,0);
   }
 }
 
