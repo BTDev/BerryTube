f2bd9c10c44ba7d1f5be1171b1896f7400f726e0
Atte Virtanen <aten.email@gmail.com>
2024-09-20 21:11:04 +0300

    maybe fix

--- web/plugins/toastthemes/css/btcon2024/theme.js
+++ web/plugins/toastthemes/css/btcon2024/theme.js
@@ -43,7 +43,7 @@
     .join("");
 
   const html = `
-    <div class="btcon2-marquee-container">
+    <div class="btcon2-marquee-container toastthemes-theme-specific">
       <div class="btcon2-marquee">
         <div class="btcon2-marquee-content"> 
           ${picsHtml}
@@ -53,8 +53,7 @@
     </div>
   `;
 
-
   whenExists("#videobg", (videobg) => {
-    $("#videobg").prepend(html);
+    videobg.prepend(html);
   });
 })();
