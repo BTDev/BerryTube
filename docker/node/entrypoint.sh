8cbd4bfe735a9424ba3f34f6aef02ca382ee391d
Atte Virtanen <git@atte.fi>
2026-08-15 13:38:04 +0300

    update Docker base images

--- docker/node/entrypoint.sh
+++ docker/node/entrypoint.sh
@@ -3,11 +3,10 @@ set -eu
 
 # Wait for MySQL to be up, otherwise the server gets confused.
 # Docker healthcheck isn't enough during server reboot.
-while ! mysql -hmysql -uberrytube -p"${MYSQL_PASSWORD:-berrytube}" -e 'SELECT "Checking MySQL...";' berrytube; do
+while ! mariadb --skip-ssl-verify-server-cert -hmysql -uberrytube -p"${MYSQL_PASSWORD:-berrytube}" -e 'SELECT "Checking MySQL...";' berrytube; do
     echo "Waiting for MySQL..." >&2
     sleep 1
 done
 
 echo "MySQL is up! Launching the tube..." >&2
 exec "$@"
-
